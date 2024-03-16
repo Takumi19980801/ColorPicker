3d73fff87be5bb0de5ef42405054674945db9e91
Takumi19980801 <takunomi1998@gmail.com>
2024-03-16 18:29:54 +0900

    定数を使用

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent = `カラーコード:${document.querySelector
-('#colorPicker').value}`;
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+text.textContent = `カラーコード:${color.value}`;
