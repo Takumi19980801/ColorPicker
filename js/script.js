824c6a64a79e7fe15a9b15164f0fb87ddb40cef4
Takumi19980801 <takunomi1998@gmail.com>
2024-03-19 22:21:43 +0900

    カラーコードコメント作成

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 
 const colorBg = () => {
+
+  // カラーコード表示
   text.textContent = `カラーコード:${color.value}`;
 }
 
-color.addEventListener('input',colorBg);
+// カラーピッカーが変更されたら colorBg を表示させる
+color.addEventListener('input',colorBg);示
